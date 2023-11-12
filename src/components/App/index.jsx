504cdaae7d227d0ae8113848431a23054e979ad4
nicolasegpla <nicolasedixongarciapineros@gmail.com>
2023-11-12 17:04:36 -0500

    LigasPopulares - creacion de componente, insertado en el home de el componenete app

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,12 @@
 import { BarraNavegacionHorizontal } from '../BarraNavegacionHorizontal'
+import { LigasPopulares } from '../LigasPopulares'
 import './_app.scss'
 
 function App() {
-  
-
   return (
     <>
       <BarraNavegacionHorizontal />
+      <LigasPopulares />
     </>
   )
 }
---
+++ src/components/LigasPopulares/index.jsx
@@ -0,0 +1,12 @@
+import './ligaspopulares.scss';
+function LigasPopulares () {
+    return (
+        <>
+            <section className='section--ligas-populares'>
+
+            </section>
+        </>
+    )
+}
+
+export { LigasPopulares }
